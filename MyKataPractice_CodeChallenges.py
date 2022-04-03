7fd643dcf84807d70e1e760ae6f023403af43f55
Bob Hartley <roberthartley09@gmail.com>
2022-04-03 15:00:59 -0500

    Update MyKataPractice_CodeChallenges.py

--- MyKataPractice_CodeChallenges.py
+++ MyKataPractice_CodeChallenges.py
@@ -62,3 +62,14 @@ def array_diff(a, b):
 def sum_two_smallest_numbers(numbers):
     numbers.sort()
     return numbers[0] + numbers[1]
+
+# A function to determine if a year is a leap year
+def is_leap(year):
+    leap = False
+    if year % 400 == 0:
+        leap = True
+    elif year % 100 != 0 and year % 4 == 0:
+        leap = True
+    else:
+        leap = False
+    return leap
